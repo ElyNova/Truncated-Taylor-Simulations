1402726eef37465681b6410036866b9a6438b5a3
ElyNova <eaanova@hotmail.com>
2018-07-02 09:57:47 -0400

    Update aXbYk1.py

--- aXbYk1.py
+++ aXbYk1.py
@@ -16,15 +16,21 @@ cr = ClassicalRegister(3)
 qc = QuantumCircuit(qr, cr)
 
 #-WRW*RW|0>|psi> = |0>U|psi>
-#First Apply W = B select(v) B*
-#Rotation U1 of B circuit
+
+#THE W CIRCUIT: W = B select(v) B*
+#Rotation U1 of B subcircuit
 theta = math.acos(math.sqrt(1/(1+math.sqrt(t))))
-qc.u3(theta, 0, math.pi,1)
+qc.u3(theta, 0, math.pi,qr(1))
 
+#select(v) subcircuit - super not correct yet
+#Need to figure out double-controlled pauli gates
+#Also, david, did you figure out how to do -i multiplier? do we use the general u3 gate?
+#I know what I am writing is not remotely correct, I just wanna test that circuit_drawer still works
+qc.cz(qr(2), qr(3))
+qc.cx(qr(1), qr(2))
+qc.cx(qr(2), qr(3))
 
+#B dagger
+qc.u3(theta, 0, math.pi, qr(1))
 
-
-
-#select(v)
-
-
+circuit_drawer(qc)
